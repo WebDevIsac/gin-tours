30bce96449e8df2d86ba674de5e65832ef78cbf9
Isac Larsson <isaclarsson98@gmail.com>
2021-06-16 22:23:56 +0200

    Some init fixes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,20 @@
+const path = require('path');
+
 module.exports = {
-  siteMetadata: {
-    title: "Gin Tours",
-  },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-gatsby-cloud"],
+    siteMetadata: {
+        title: 'Gin Tours',
+        description: 'Book your tailored gin tour now',
+    },
+    plugins: [
+        'gatsby-plugin-emotion',
+        {
+            resolve: 'gatsby-plugin-root-import',
+            options: {
+                src: path.join(__dirname, 'src'),
+                pages: path.join(__dirname, 'src/pages'),
+                config: path.join(__dirname, 'src/config'),
+                components: path.join(__dirname, 'src/components'),
+            },
+        },
+    ],
 };
