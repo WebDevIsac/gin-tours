58b48bb3fbd6351dc736b93e34a6160ba21b9c5d
Isac Larsson <isaclarsson98@gmail.com>
2021-07-06 23:59:24 +0200

    Add ratio and fix styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,38 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import images from 'src/images/destilleries';
 
 const colors = ['burlywood', 'thistle', 'lightblue', 'salmon'];
 
 const CardRow = styled('div')`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    height: 60vh;
     position: relative;
+    width: 100%;
+    border: 1px solid black;
+
+    &:after {
+        content: '';
+        display: block;
+        padding-bottom: 150%;
+    }
+
+    & > div {
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+    }
+`;
+
+const Column = styled('div')`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
     background-color: ${({ index }) => colors[index]};
+`;
+
+const Image = styled('img')`
+    width: 100%;
+    height: 70%;
+    object-fit: cover;
 `;
 
 const Box = styled('div')`
     display: flex;
     flex-direction: column;
-    width: 500px;
-    height: 200px;
     padding: 32px;
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    color: white;
-    background-color: grey;
+    width: 100%;
+    height: 30%;
+    color: black;
+    border-top: 1px solid black;
+    flex: 1 0 auto;
+
+    & > span {
+        color: grey;
+    }
 `;
 
-const Card = ({ name, index }) => {
+const Card = ({ name, place, image, index }) => {
     return (
         <CardRow index={index}>
-            <Box>
-                <h3>Titel: {name}</h3>
-                <p>Bildtext</p>
-            </Box>
+            <Column>
+                <Image src={images[image]} />
+                <Box>
+                    <span>Plats: {place}</span>
+                    <h3>Titel: {name}</h3>
+                    <p>Bildtext</p>
+                </Box>
+            </Column>
         </CardRow>
     );
 };
