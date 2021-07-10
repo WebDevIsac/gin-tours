ae8f5d77bcdfb84af95220be4a585b72cc9f3bbb
Isac Larsson <isaclarsson98@gmail.com>
2021-07-10 15:20:15 +0200

    Create mobile menu

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import { StaticQuery, graphql } from 'gatsby';
+import { StaticQuery, graphql, Link } from 'gatsby';
 import styled from '@emotion/styled';
-import { Link } from 'gatsby';
+import { above, below } from 'src/util/mediaqueries';
 import colors from 'config/colors';
 
 const Navbar = styled('div')`
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    right: 0;
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between;
     padding: 16px 32px;
     border-bottom: 1px solid ${colors.black};
+    background-color: white;
 `;
 
 const Logotype = styled('div')`
@@ -19,10 +25,46 @@ const Logotype = styled('div')`
     line-height: 1em;
 `;
 
+const Hamburger = styled('div')`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 40px;
+    height: 24px;
+    position: relative;
+    z-index: 3;
+
+    ${above.md} {
+        display: none;
+    }
+`;
+
+const Line = styled('span')`
+    display: inline-block;
+    width: 100%;
+    height: 2px;
+    background-color: black;
+`;
+
 const Menu = styled('div')`
     display: flex;
     flex-direction: row;
     align-items: center;
+
+    ${below.md} {
+        flex-direction: column;
+        position: absolute;
+        z-index: 2;
+        top: 0;
+        left: 0;
+        right: 0;
+        transform: translateY(-100%);
+        background-color: white;
+
+        &.is-open {
+            transform: translateY(0);
+        }
+    }
 `;
 
 const Item = styled(Link)`
@@ -31,19 +73,31 @@ const Item = styled(Link)`
     line-height: 1em;
 `;
 
-const Header = () => {
-    const query = graphql`
-        query {
-            allMenuItemsJson {
-                edges {
-                    node {
-                        title
-                        slug
-                    }
+const query = graphql`
+    query {
+        allMenuItemsJson {
+            edges {
+                node {
+                    title
+                    slug
                 }
             }
         }
-    `;
+    }
+`;
+
+const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleMenuState = (justClose = false) => {
+        if (justClose && isOpen) {
+            setIsOpen(false);
+
+            return;
+        }
+
+        setIsOpen(!isOpen);
+    };
 
     return (
         <StaticQuery
@@ -53,9 +107,16 @@ const Header = () => {
                     <Logotype>
                         <Link to="/">GIN TOURS</Link>
                     </Logotype>
-                    <Menu>
+                    <Hamburger onClick={handleMenuState}>
+                        <Line />
+                        <Line />
+                        <Line />
+                    </Hamburger>
+                    <Menu className={isOpen ? 'is-open' : ''}>
                         {data.allMenuItemsJson.edges.map(({ node: { title, slug } }) => (
-                            <Item to={slug}>{title}</Item>
+                            <Item to={slug} key={title} onClick={() => handleMenuState(true)}>
+                                {title}
+                            </Item>
                         ))}
                     </Menu>
                 </Navbar>
