33d5cef6ab4c115b2de13487b8ad93ca63a035cc
Isac Larsson <isaclarsson98@gmail.com>
2021-08-29 23:20:58 +0200

    Install slider pkg and create component

---
+++ src/components/Slider.js
@@ -0,0 +1,37 @@
+import React, { useRef } from 'react';
+import PropTypes from 'prop-types';
+import Glide from 'react-glidejs';
+
+import 'react-glidejs/dist/index.css';
+
+const Slider = ({ children }) => {
+    const gliderRef = useRef(null);
+
+    return (
+        <Glide
+            ref={gliderRef}
+            throttle={0}
+            type="carousel"
+            customSlideAnimation={{
+                timeout: 500,
+                classNames: 'fade',
+            }}
+            peek={{
+                before: 500,
+                after: 500,
+            }}
+            perView={1}
+            startAt={0}
+            slideClassName="slider__frame"
+            focusAt="center"
+        >
+            {children}
+        </Glide>
+    );
+};
+
+Slider.propTypes = {
+    children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+};
+
+export default Slider;
