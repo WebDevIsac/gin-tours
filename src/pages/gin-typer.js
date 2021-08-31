2b0ed7cdfbf2cf55b016cfb64fdf513a024be625
Isac Larsson <isaclarsson98@gmail.com>
2021-08-31 22:28:36 +0200

    Change text max widths and paddings

--- src/pages/gin-typer.js
+++ src/pages/gin-typer.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { StaticQuery, graphql } from 'gatsby';
+import { above } from 'util/mediaqueries';
 import Layout from 'components/layouts/Layout';
 import SEO from 'components/SEO/SEO';
 
 const Wrapper = styled('div')`
     width: 100%;
     height: 100%;
-    max-width: 800px;
+    max-width: 1200px;
+    padding: 0 16px;
+
+    ${above.md} {
+        padding: 0 32px;
+    }
 `;
 
 const Section = styled('section')``;
 
 const H3 = styled('h3')`
-    font-size: 24px;
+    font-size: 32px;
+    line-height: 1em;
 `;
 
 const Text = styled('p')`
-    font-size: 18px;
+    font-size: 20px;
+    line-height: 1em;
 `;
 
 const query = graphql`
