4397250d2ced9d012c7a0f160e50e1c72b2b03b6
Isac Larsson <isaclarsson98@gmail.com>
2021-09-12 19:48:26 +0200

    Add more content to recipe template

--- src/templates/recept.js
+++ src/templates/recept.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import styled from '@emotion/styled';
+import colors from 'config/colors';
+import { above } from 'util/mediaqueries';
 import Layout from 'components/layouts/Layout';
 import SEO from 'components/SEO/SEO';
 import images from 'images/recipes';
@@ -14,19 +16,71 @@ const Wrapper = styled('div')`
     align-items: center;
 `;
 
-const Image = styled('img')`
-    width: 25%;
+const BackgroundImage = styled('div')`
+    width: 100%;
+    height: 80vh;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: top center;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+`;
+
+const H1 = styled('h1')`
+    color: ${colors.white};
+    text-align: center;
+`;
+
+const Content = styled('div')`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 24px;
+`;
+
+const IngredientsList = styled('ul')`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+`;
+
+const Ingredient = styled('li')`
+    width: 80%;
+    text-align: left;
+    margin-bottom: 8px;
+`;
+
+const H3 = styled('h3')`
+    font-size: 24px;
+`;
+
+const Instructions = styled('p')`
+    font-size: 20px;
+    line-height: 1.2em;
 `;
 
 const Recipe = ({ data }) => {
-    const { title, image } = data.recipesJson;
+    const { title, image, ingredients, instructions } = data.recipesJson;
 
     return (
         <>
             <SEO title={title} />
             <Wrapper>
-                <h1>{title}</h1>
-                <Image src={images[image]} />
+                <BackgroundImage style={{ backgroundImage: `url(${images[image]})` }}>
+                    <H1>{title}</H1>
+                </BackgroundImage>
+                <Content>
+                    <H3>Ingredienser</H3>
+                    <IngredientsList>
+                        {ingredients.map((ingredient, index) => (
+                            <Ingredient key={index}>{ingredient}</Ingredient>
+                        ))}
+                    </IngredientsList>
+                    <H3>Instruktioner</H3>
+                    {instructions && <Instructions>{instructions}</Instructions>}
+                </Content>
             </Wrapper>
         </>
     );
@@ -37,6 +91,8 @@ export const query = graphql`
         recipesJson(slug: { eq: $slug }) {
             title
             image
+            ingredients
+            instructions
         }
     }
 `;
@@ -46,6 +102,8 @@ Recipe.propTypes = {
         recipesJson: PropTypes.shape({
             image: PropTypes.string,
             title: PropTypes.string,
+            ingredients: PropTypes.arrayOf(PropTypes.string),
+            instructions: PropTypes.string,
         }),
     }).isRequired,
 };
