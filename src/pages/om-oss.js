9ba544f4f4496ba62354a81193a5515fd5a3cad0
Isac Larsson <isaclarsson98@gmail.com>
2021-09-27 22:42:16 +0200

    Add text to about page

--- src/pages/om-oss.js
+++ src/pages/om-oss.js
@@ -8,12 +8,34 @@ const Wrapper = styled('div')`
     height: 100%;
 `;
 
+const StyledLink = styled('a')`
+    text-decoration: underline;
+`;
+
 const About = () => {
+    const title = 'Om oss';
     return (
         <>
-            <SEO title="Om oss" />
+            <SEO title={title} />
             <Wrapper>
-                <h1>Om oss</h1>
+                <h1>{title}</h1>
+                <p>Vi erbjuder resor till de främsta men även uppkommande destillerier runtom i landet</p>
+
+                <h3>Kontaktuppgifter</h3>
+                <ul>
+                    <li>
+                        Har du några frågor,{' '}
+                        <StyledLink href="mailto:kundservice@gintours.se">kontaka vår support</StyledLink>
+                    </li>
+                    <li>
+                        Specilla önskemål eller idéer,{' '}
+                        <StyledLink href="mailto:boka@gintours.se">boka via mail</StyledLink>
+                    </li>
+                    <li>
+                        Intresserad av att samarbeta med oss,{' '}
+                        <StyledLink href="mailto:boka@gintours.se">skicka ett mail</StyledLink>
+                    </li>
+                </ul>
             </Wrapper>
         </>
     );
