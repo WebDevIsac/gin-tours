2d84aa7ed34834b702637f36a52fc56c2891e593
Isac Larsson <isaclarsson98@gmail.com>
2021-10-07 19:36:22 +0200

    Add link and text to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import styled from '@emotion/styled';
 import Layout from 'components/layouts/Layout';
 import SEO from 'components/SEO/SEO';
+import mobileImage from 'images/mobile-image.jpg';
+import colors from 'config/colors';
+import { hover } from 'util/mediaqueries';
 
 const Wrapper = styled('div')`
     width: 100%;
-    height: 100%;
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-image: url(${mobileImage});
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: 50% 30%;
+    color: ${colors.white};
+`;
+
+const StyledLink = styled(Link)`
+    font-size: 24px;
+    text-decoration: underline;
+
+    ${hover} {
+        transition: opacity 200ms ease;
+        &:hover {
+            opacity: 0.8;
+        }
+    }
 `;
 
 const NotFoundPage = () => {
@@ -14,6 +39,7 @@ const NotFoundPage = () => {
             <SEO title="404 - Sidan kunde inte hittas" />
             <Wrapper>
                 <h1>404 - Sidan kunde inte hittas</h1>
+                <StyledLink to="/">Gå till startsidan</StyledLink>
             </Wrapper>
         </>
     );
