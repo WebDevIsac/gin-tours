f920bebac81de80bfda0098fc0489921438adce1
Isac Larsson <isaclarsson98@gmail.com>
2021-11-21 16:32:18 +0100

    Add hernö recipes

--- src/images/recipes/index.js
+++ src/images/recipes/index.js
@@ -6,6 +6,13 @@ import blackflip from './blackflip.jpeg';
 import callItSpritz from './call-it-spritz.jpeg';
 import lillablackan from './lillablackan.jpeg';
 import pickledMidsummer from './pickled-midsummer.jpeg';
+import hernoGinFlip from './herno-gin-flip.jpeg';
+import ramosGinFizz from './ramos-gin-fizz.jpeg';
+import beesKnees from './bees-knees.jpeg';
+import hernoBasilSmash from './herno-basil-smash.jpeg';
+import negroniWarmer from './negroni-warmer.jpeg';
+import tomCollins from './tom-collins.jpeg';
+import hernoGinFlipRaspberry from './herno-gin-flip-raspberry.jpeg';
 
 export default {
     drink,
@@ -16,4 +23,11 @@ export default {
     callItSpritz,
     lillablackan,
     pickledMidsummer,
+    hernoGinFlip,
+    ramosGinFizz,
+    beesKnees,
+    hernoBasilSmash,
+    negroniWarmer,
+    tomCollins,
+    hernoGinFlipRaspberry,
 };
