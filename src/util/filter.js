96900007e24c982620e335920a87d3326218d357
Isac Larsson <isaclarsson98@gmail.com>
2022-02-04 23:42:37 +0100

    Refactor recipe page to handle filter with navigation

--- src/util/filter.js
+++ src/util/filter.js
@@ -1,132 +1,44 @@
 import { navigate } from 'gatsby';
 
-const config = ['distillery'];
+const config = ['destilleri', 'gin-typ'];
 
-export const decodeString = string => {
-    if (!string || typeof string !== 'string') {
-        return null;
-    }
+export const getParamFilter = path => {
+    const splittedPath = path.split('/');
+    const indices = config.map(conf => splittedPath.indexOf(conf)).filter(index => index !== -1);
 
-    string = string.toLowerCase();
-
-    string = string.replace(/ /g, '-');
-
-    const entities = [
-        ['amp', '&'],
-        ['#038', '&'],
-        ['apos', "'"],
-        ['#x27', "'"],
-        ['#x2F', '/'],
-        ['#39', "'"],
-        ['#8217', "'"],
-        ['#47', '/'],
-        ['lt', '<'],
-        ['gt', '>'],
-        ['nbsp', ' '],
-        ['quot', '"'],
-        ['&ndash;', '-'],
-        ['#8211', '-'],
-        ['shy', ''],
-        ['%C3%A5', 'å'],
-        ['%C3%A4', 'ä'],
-        ['%C3%B6', 'ö'],
-    ];
-    for (let i = 0, max = entities.length; i < max; ++i) {
-        string = string.replace(new RegExp(`&${entities[i][0]};`, 'g'), entities[i][1]);
-    }
-
-    return string;
-};
-
-export const getParamResults = (allRecipes, search) => {
     const filter = {};
-
-    if (!search) {
-        return [allRecipes, filter];
-    }
-
-    const urlParams = new URLSearchParams(search);
-    const paramKeys = urlParams.keys();
-
-    let filteredRecipes = [];
-    let index = 0;
-
-    for (const key of paramKeys) {
-        if (!config.includes(key)) {
-            continue;
+    indices.forEach(index => {
+        if (splittedPath[index + 1]) {
+            filter[splittedPath[index]] = splittedPath[index + 1]?.replace(':', '');
         }
+    });
 
-        const values = urlParams.get(key);
-        const valuesArray = values.split(',');
-        filter[key] = valuesArray;
-
-        const recipes = (index === 0 ? allRecipes : filteredRecipes).filter(recipe =>
-            valuesArray.includes(recipe[key])
-        );
-
-        index++;
-        filteredRecipes = recipes;
-    }
-
-    return [filteredRecipes, filter];
+    return filter;
 };
 
-export const setFilterParams = (value, key, allRecipes, filter) => {
-    const decodedValue = decodeString(value);
-    const matchedFilter = filter[key];
+export const setFilterParams = (value, currentKey, filter) => {
+    const encodedValue = value.toLowerCase().replace(/ /g, '-');
 
-    if (matchedFilter) {
-        const valueIndex = matchedFilter.indexOf(decodedValue);
-        if (valueIndex !== -1) {
-            if (matchedFilter.length > 1) {
-                filter[key].splice(valueIndex, 1);
-            } else {
-                delete filter[key];
-            }
-        } else {
-            filter[key].push(decodedValue);
-        }
+    if (filter[currentKey] === encodedValue) {
+        delete filter[currentKey];
     } else {
-        filter[key] = [decodedValue];
+        filter[currentKey] = encodedValue;
     }
+    const filterKeys = Object.keys(filter || {})
+        .filter(key => config.includes(key))
+        .sort((a, b) => config.indexOf(a) - config.indexOf(b));
+    let paramQuery = '';
 
-    let index = 0;
-    let filteredRecipes = [];
-    const activeFilterKeys = Object.keys(filter);
-
-    if (!activeFilterKeys?.length) {
-        navigate('/recept/', {
-            replace: true,
-            state: {
-                disableScrollUpdate: true,
-            },
-        });
-
-        return [allRecipes, filter];
-    }
-
-    activeFilterKeys.forEach(filterKey => {
-        const valuesArray = filter[filterKey];
-        const recipes = (index === 0 ? allRecipes : filteredRecipes).filter(recipe =>
-            valuesArray.includes(recipe[filterKey])
-        );
-
-        index++;
-        filteredRecipes = recipes;
+    filterKeys.map(key => {
+        paramQuery += `${key}/:${filter[key]}/`;
     });
 
-    let params = '';
-    activeFilterKeys.forEach((filterKey, index) => {
-        const valueString = filter[filterKey].join(',');
-        params += `${index === 0 ? '?' : '&'}${filterKey}=${valueString}`;
-    });
-
-    navigate(`/recept/${params}`, {
+    navigate(`/recept/${paramQuery}`, {
         replace: true,
         state: {
-            disableScrollUpdate: true,
+            disableScrollUpdate: window.scrollY > 83,
         },
     });
 
-    return [filteredRecipes, filter];
+    return filter;
 };
